33fa176b0ee86f7eb14cb9f237dc3078e66f3dd5
EavOutDom <eavoutdom400@gmail.com>
2022-07-02 22:21:03 +0700

    update 4

--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,7 @@ import Reviews from "./reviews";
 function App() {
     return (
         <div className="grid place-items-center">
-            <div className="font-bold mt-4 text-2xl">Reviews</div>
+            <div className="font-bold mt-4 text-xl md:text-2xl duration-500">Reviews</div>
             <div className="h-1 w-28 bg-black mx-auto"></div>
             <Reviews />
         </div>
--- src/reviews.jsx
+++ src/reviews.jsx
@@ -20,22 +20,21 @@ const Reviews = () => {
     };
     const random = () => {
         let ran = Math.floor(Math.random() * data.length);
-        if(ran === review){
+        if (ran === review) {
             ran = review + 1;
-
         }
-        setReview(checkIndex(ran))
+        setReview(checkIndex(ran));
         console.log(ran);
     };
     return (
         <div
-            className="bg-white w-[38rem] h-96 text-center my-5
+            className="bg-white md:w-[38rem] w-80 duration-500 px-2 text-center my-5
             shadow-lg p-3 transition-all delay-300 ease-in
             rounded-md hover:shadow-2xl hover:transition-all 
             hover:delay-300 hover:ease-out"
         >
             <div className="flex justify-center">
-                <div className="relative w-32 h-32 rounded-full">
+                <div className="relative md:w-32 md:h-32 duration-500 w-20 h-20 rounded-full">
                     <img
                         src={image}
                         alt={name}
@@ -44,12 +43,12 @@ const Reviews = () => {
                 </div>
             </div>
             <h2 className="text-lg font-bold">{name}</h2>
-            <h4>{job}</h4>
+            <h4 className={'underline'}>{job}</h4>
             <p>{text}</p>
             <div className="my-3 flex items-center justify-center">
-                <div className="w-20 flex justify-between">
+                <div className="w-full px-10 flex justify-between">
                     <button
-                        className="text-2xl cursor-pointer hover:text-slate-500"
+                        className="text-5xl md:text-2xl duration-500  md:hover:text-slate-500"
                         onClick={() => {
                             setReview((index) => {
                                 const newIndex = index - 1;
@@ -60,7 +59,7 @@ const Reviews = () => {
                         <BsFillArrowLeftCircleFill />
                     </button>
                     <button
-                        className="text-2xl cursor-pointer hover:text-slate-500"
+                        className="text-5xl md:text-2xl duration-500 md:hover:text-slate-500"
                         onClick={() => {
                             setReview((index) => {
                                 const newIndex = index + 1;
@@ -74,7 +73,7 @@ const Reviews = () => {
             </div>
             <button
                 className="ring-2 focus:ring-2 p-2 m-1 ring-black transition-all delay-200 ease-out
-             hover:bg-gray-900 hover:text-white hover:transition-all hover:delay-200 hover:ease-in"
+             md:hover:bg-gray-900 md:hover:text-white md:hover:transition-all md:hover:delay-200 md:hover:ease-in"
                 onClick={random}
             >
                 RANDOM
